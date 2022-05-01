8ac51f86b0665b8e49201d9b9ef8b4585e3db13b
guitrentini96 <guitrentini96@gmail.com>
2022-05-01 23:34:24 +0200

    basic project with container set

---
+++ src/components/GeneralContainer.js
@@ -0,0 +1,22 @@
+import { Container, Paper } from '@mui/material';
+
+const GeneralContainer = ({ children }) => {
+  return (
+    <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Paper
+        variant="outlined"
+        sx={{
+          width: '20rem',
+          backgroundColor: 'beige',
+          marginTop: '10vh',
+          height: '80vh',
+          padding: '10px',
+        }}
+      >
+        {children}
+      </Paper>
+    </Container>
+  );
+};
+
+export default GeneralContainer;
